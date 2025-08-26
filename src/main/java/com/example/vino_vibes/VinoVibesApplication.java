a97fbc91b16144f3a0bd751608729cfa73f17748
AnaCarinaPP <ancari1989@gmail.com>
2025-08-26 12:13:25 +0200

    first commit

---
+++ src/main/java/com/example/vino_vibes/VinoVibesApplication.java
@@ -0,0 +1,13 @@
+package com.example.vino_vibes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinoVibesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinoVibesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vino_vibes/VinoVibesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vino_vibes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinoVibesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
