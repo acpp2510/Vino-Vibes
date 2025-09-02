df54e8a0874a9af47db2aa89b474c2670b27f86b
AnaCarinaPP <ancari1989@gmail.com>
2025-09-02 20:13:15 +0200

    feat(WineRepository-WineService): update repository with add all methods of wineservice

--- src/main/java/com/example/vino_vibes/repositories/WineRepository.java
+++ src/main/java/com/example/vino_vibes/repositories/WineRepository.java
@@ -3,5 +3,16 @@ package com.example.vino_vibes.repositories;
 import com.example.vino_vibes.models.Wine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WineRepository  extends JpaRepository <Wine, Long> {
+    List<Wine> findByUserId(Long userId);
+
+    Boolean existsByWineNameAndWineProducerAndYear(String wineName, String wineProducer, Integer year);
+
+    List<Wine> findByRegion(String region);
+
+    List<Wine> findByWineType(String wineType);
+
+    List<Wine> findByWineProducer(String wineProducer);
 }
